941faf5669165f619923e0c02cf850308685f365
AdrianDiaz <cesarqqu@gmail.com>
2023-03-21 14:29:20 -0600

    last changes

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
+const scrollTopbutton = document.getElementById('scroll-top-button')
 const btnForm = document.getElementById('btn-form')
 const inputForm = document.getElementById('input-form')
 const alertSucces= document.getElementById('alert-succes')
 const alertFail = document.getElementById('alert-fail')
 const regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
 
-btnForm.addEventListener('click', e => {
-    e.preventDefault()
-    
-    if(regex.test(inputForm.value)) {
-        showAlert(alertSucces)
-        setTimeout(() => {
-            showAlert(alertSucces)
-        },3000)
-    }else {
-        showAlert(alertFail)
-        setTimeout(() => {
-            showAlert(alertFail)
-        },3000)
-    }
-})
 
 function showAlert(alert){
     if (alert.classList.contains('start-50')) {
@@ -30,4 +16,33 @@ function showAlert(alert){
     }
 }
 
-AOS.init()
+    
+const onScroll = (e) => {
+        const scrollposition = e.target.scrollingElement.scrollTop
+        scrollTopbutton.classList.toggle('visible', scrollposition > 0)
+}
+const scrolltoTop = () => {
+        window.scrollTo({
+            top: 0, 
+            behavior: 'smooth'
+        })
+}
+    document.addEventListener('scroll', onScroll)
+    scrollTopbutton.addEventListener('click', scrolltoTop)
+    btnForm.addEventListener('click', e => {
+        e.preventDefault()
+        
+        if(regex.test(inputForm.value)) {
+            showAlert(alertSucces)
+            setTimeout(() => {
+                showAlert(alertSucces)
+            },3000)
+        }else {
+            showAlert(alertFail)
+            setTimeout(() => {
+                showAlert(alertFail)
+            },3000)
+        }
+})
+    
+    AOS.init()
